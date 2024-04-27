2521e4bc22e114db2b145a9c41172695c3a3497f
XiaoMoMi <972454774@qq.com>
2024-04-27 20:33:25 +0800

    Fix default configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 allprojects {
 
     project.group = "net.momirealms"
-    project.version = "3.4.5.1"
+    project.version = "3.4.5.2"
 
     apply<JavaPlugin>()
     apply(plugin = "java")
