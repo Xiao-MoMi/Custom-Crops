fed8a2229bcf97afc2e2693d6c93314282013eec
XiaoMoMi <972454774@qq.com>
2024-07-22 23:41:45 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 allprojects {
 
     project.group = "net.momirealms"
-    project.version = "3.5.7.3"
+    project.version = "3.5.7.4"
 
     apply<JavaPlugin>()
     apply(plugin = "java")
