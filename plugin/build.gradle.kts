e4d8087c21764237e55468d8417360bb176eea11
XiaoMoMi <littlemomi@qq.com>
2025-05-01 11:01:38 +0800

    bump version

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
         exclude("com.google.code.gson", "gson")
     }
     implementation("net.kyori:adventure-text-serializer-legacy:${rootProject.properties["adventure_bundle_version"]}")
-    implementation("com.github.Xiao-MoMi:AntiGriefLib:${rootProject.properties["anti_grief_version"]}")
+    implementation("net.momirealms:antigrieflib:${rootProject.properties["anti_grief_version"]}")
     implementation("net.momirealms:sparrow-heart:${rootProject.properties["sparrow_heart_version"]}")
     implementation("com.saicone.rtag:rtag:${rootProject.properties["rtag_version"]}")
     implementation("com.saicone.rtag:rtag-item:${rootProject.properties["rtag_version"]}")
