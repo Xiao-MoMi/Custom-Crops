084974acfef375cac5972238a55437039d01a868
Jimin Seo <aa090402@naver.com>
2026-01-02 18:15:45 +0900

    add sprinkler

---
+++ compatibility/src/main/java/net/momirealms/customcrops/bukkit/integration/quest/bq/sprinkler/BreakSprinklerObjectiveFactory.java
@@ -0,0 +1,19 @@
+package net.momirealms.customcrops.bukkit.integration.quest.bq.sprinkler;
+
+import org.betonquest.betonquest.api.DefaultObjective;
+import org.betonquest.betonquest.api.QuestException;
+import org.betonquest.betonquest.api.instruction.Argument;
+import org.betonquest.betonquest.api.instruction.Instruction;
+import org.betonquest.betonquest.api.quest.objective.ObjectiveFactory;
+
+import java.util.List;
+
+public class BreakSprinklerObjectiveFactory implements ObjectiveFactory {
+
+    @Override
+    public DefaultObjective parseInstruction(Instruction instruction) throws QuestException {
+        final Argument<List<String>> names = instruction.string().list().get();
+        final Argument<Number> targetAmount = instruction.number().get("amount", 1);
+        return new BreakSprinklerObjective(instruction, targetAmount, names);
+    }
+}
